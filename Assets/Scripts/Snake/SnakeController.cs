db890289ad4a0ce6cd914fb72f9f9b2fed4fadbf
AER-S <89151307+AER-S@users.noreply.github.com>
2021-11-25 00:47:14 +0100

    Spawning the snake through code

--- Assets/Scripts/Snake/SnakeController.cs
+++ Assets/Scripts/Snake/SnakeController.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SnakePartController snakePartPrefab;
+    [SerializeField] private int snakePartsStartingNumber = 2;
+    private List<SnakePartController> snakeParts=new List<SnakePartController>();
+    private float partSize;
+
+    private void Awake()
     {
-        
+        partSize = snakePartPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        CheckSnakePartsSatringNumber();
+        StartSnake();
+    }
+
+    void CheckSnakePartsSatringNumber()
+    {
+        snakePartsStartingNumber = (snakePartsStartingNumber < 2) ? 2 : snakePartsStartingNumber;
+    }
+
+    void AddNewSnakePart(int _index, Vector3 _position)
+    {
+        SnakePartController newSnakePart = Instantiate(snakePartPrefab, this.transform, false);
+        newSnakePart.transform.position = _position;
+        snakeParts.Insert(_index,newSnakePart);
+    }
+
+    void StartSnake()
+    {
+        for (int i = 0; i < snakePartsStartingNumber; i++)
+        {
+            Vector3 newPosition = new Vector3(-i * partSize, 0f, 0f);
+            AddNewSnakePart(i, newPosition);
+        }
     }
 }
