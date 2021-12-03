2f930b3510ea960843d229e78931d37dfadcf561
AER-S <89151307+AER-S@users.noreply.github.com>
2021-12-03 14:39:07 +0100

    Making Biting a snake part destroy a shield

--- Assets/Scripts/Snake/SnakePartController.cs
+++ Assets/Scripts/Snake/SnakePartController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakePartController : BaseObstacle
@@ -8,8 +10,25 @@ public class SnakePartController : BaseObstacle
 
     public override void HinderSnake()
     {
-        Debug.Log("hitting itself");
-        //SnakeController.Instance.KillSnake();
-        //Decrezsehealt();
+        StartCoroutine(StopSnakePartCollision());
+        SnakeController.Instance.HitSnake("Snake Part",1);
+        
+
+    }
+
+    IEnumerator StopSnakePartCollision()
+    {
+        
+        List<SnakePartController> snakeParts = SnakeController.Instance.GetSnakeParts();
+        foreach (SnakePartController snakePart in snakeParts)
+        {
+            snakePart.gameObject.layer = 0;
+        }
+
+        yield return new WaitForSecondsRealtime(SnakeController.Instance.GetSpeed());
+        foreach (SnakePartController snakePart in snakeParts)
+        {
+            snakePart.gameObject.layer = 7;
+        }
     }
 }
